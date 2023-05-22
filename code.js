76f28e3744c107b9a7cc28a94672a9f6b4142efb
Melkcampos <melquisedequecampos.trabalho@gmail.com>
2023-05-21 21:35:56 -0300

    bot working well

--- code.js
+++ code.js
@@ -1,54 +1,54 @@
 
-const chatLog = document.getElementById('chat-log')
-userInput = document.getElementById('user-input')
-sendButton = document.getElementById('send-button')
-buttonIcon = document.getElementById('button-icon')
-info = document.querySelector('.info')
+const chatLog = document.getElementById('chat-log'),
+    userInput = document.getElementById('user-input'),
+    sendButton = document.getElementById('send-button'),
+    buttonIcon = document.getElementById('button-icon'),
+    info = document.querySelector('.info');
 
-sendButton.addEventListener('click', sendMessage)
+sendButton.addEventListener('click', sendMessage);
 
 userInput.addEventListener('keydown', (event) => {
 
-    if(event.key === 'Enter') {
-        sendMessage()
+    if (event.key === 'Enter') {
+        sendMessage();
     }
-})
+});
 
 function sendMessage() {
 
-    const message = userInput.value.trim()
+    const message = userInput.value.trim();
 
     // se não houver mensagem o programa não faz nada
-    if(message === '') {
-        return
-    } 
-    
-    // se for digitado 'developer' o chat ira mostrarm o nome do criador
-    else if(message === 'developer'){
+    if (message === '') {
+        return;
+    }
+   // se for digitado 'developer' o chat ira mostrarm o nome do criador
+    else if (message === 'developer') {
 
         // limpando o valor do 'input'
-        userInput.value = '' 
-        appendMessage('user', message)
+        userInput.value = '';
+        appendMessage('user', message);
 
-        // cria um timeout fake mostrando o carregamento no envio da mensagem atravez do button
+      // cria um timeout fake mostrando o carregamento no envio da mensagem atravez do button
         setTimeout(() => {
+
             // programando função para a palavra 'bot' quando for digitada
-            appendMessage('bot', 'This source coded by Melquisedeque F. Campos')
-            buttonIcon.classList.add('fa-solid', 'fa-paper-plane') 
+            appendMessage('bot', 'This source coded by Melquisedeque F. Campos');
 
             // muda o icone do botão para padrão
-            buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse')
-        }, 2000)
-        return
+            buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+            buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
+        }, 2000);
+        return;
     }
 
     // caso contrário, se nenhum dos itens a cima 
     //anexar a mensagem do usuário à tela
-
-    appendMessage('user', message)
-    userInput.value = ''
+    appendMessage('user', message);
+    userInput.value = '';
 
     const options = {
+
         method: 'POST',
         headers: {
             'content-type': 'application/json',
@@ -56,15 +56,60 @@ function sendMessage() {
             'X-RapidAPI-Host': 'chatgpt53.p.rapidapi.com'
         },
         body: `{"messages":[{"role":"user","content":"${message}"}]}`
-    }
+    };
 
+    fetch('https://chatgpt53.p.rapidapi.com/', options).then((response) => response.json()).then((response) => {
+        appendMessage('bot', response.choices[0].message.content);
 
+        buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+        buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
 
+    }).catch((error) => {
 
-
+        if (error.name === 'TypeError') {
+            appendMessage('bot', 'Error : Check Your Api Key!');
+            buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+            buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
+        }
+    });
 }
 
+function appendMessage(sender, message) {
+    
+    info.style.display = "none";
 
+    // muda o botão para 'loading' usando a biblioteca do fontawesome
+    buttonIcon.classList.remove('fa-solid', 'fa-paper-plane');
+    buttonIcon.classList.add('fas', 'fa-spinner', 'fa-pulse');
 
+    const messageElement = document.createElement('div');
+    const iconElement = document.createElement('div');
+    const chatElement = document.createElement('div');
+    const icon = document.createElement('i');
+
+    chatElement.classList.add("chat-box");
+    iconElement.classList.add("icon");
+    messageElement.classList.add(sender);
+    messageElement.innerText = message;
+
+     // adicione ícones dependendo de quem envia a mensagem bot ou usuário
+
+    if (sender === 'user') {
+        icon.classList.add('fa-regular', 'fa-user');
+        iconElement.setAttribute('id', 'user-icon');
+    } 
+    
+    else {
+        icon.classList.add('fa-solid', 'fa-robot');
+        iconElement.setAttribute('id', 'bot-icon');
+    }
+
+    iconElement.appendChild(icon);
+    chatElement.appendChild(iconElement);
+    chatElement.appendChild(messageElement);
+    chatLog.appendChild(chatElement);
+    chatLog.scrollTo = chatLog.scrollHeight;
+
+}
 
 
